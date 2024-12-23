f88c97d0ad3f0971131704662e278eb5c19ff38d
TKMZJY <49679124+TKMZJY@users.noreply.github.com>
2024-12-23 17:02:32 +0800

    优化

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [
+        vue(),    
+    ],
+    server: {
+        host: '0.0.0.0'
+    },
+    define: {
+        'process.env': {},
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: false,
+        __INTLIFY_PROD_DEVTOOLS__: false
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: process.env.NODE_ENV === 'production' ? '/z-tools/' : '/'
+})
